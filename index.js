590d21486c9d839fc9bb2845d0fa8bb9213d2b27
merritske <merritske@gmail.com>
2025-04-02 13:40:38 +0200

    takenlijst

--- index.js
+++ index.js
@@ -1,60 +1,83 @@
+//EVENT aanmaken
 let opslaanBtn = document.getElementById("evenement")
 let evenet = document.getElementById("eventnaam")
 let eventcontainer = document.getElementById("evenementgegevens")
 let waar = document.getElementById("waar")
 let wanneer = document.getElementById("wanneer")
-let eventGegevens= []
+let eventGegevens = []
 let eventTonen = document.getElementById("gegevensTonen")
 
-function opslaan(){
+function opslaan() {
 
-  eventGegevens.push({wat:evenet.value, waar:waar.value, wanneer:wanneer.value})
-    localStorage.setItem("evenementContainer", JSON.stringify(eventGegevens) )
-    console.log(eventGegevens)
+  eventGegevens.push({ wat: evenet.value, waar: waar.value, wanneer: wanneer.value })
+  localStorage.setItem("evenementContainer", JSON.stringify(eventGegevens))
+  console.log(eventGegevens)
   opslaanBtn.style.display = "none"
-    if( opslaanBtn.style.display = "none"){
-//      let txtI = document.createElement("h1")
-//      let txtO = document.createTextNode(`${evenet.value}`)
-// txtI.appendChild(txtO)
-console.log(waar.value, wanneer.value)
-// eventcontainer.innerHTML += `
-// <h1>${evenet.value}</h1>
-// <ul style="list-style-type=none">
-// <li>Waar: ${waar.value}</li>
-// <li>Wanneer: ${wanneer.value}</li>
-// </ul>
-// `
-    }
+  if (opslaanBtn.style.display = "none") {
+    //      let txtI = document.createElement("h1")
+    //      let txtO = document.createTextNode(`${evenet.value}`)
+    // txtI.appendChild(txtO)
+    console.log(waar.value, wanneer.value)
+    // eventcontainer.innerHTML += `
+    // <h1>${evenet.value}</h1>
+    // <ul style="list-style-type=none">
+    // <li>Waar: ${waar.value}</li>
+    // <li>Wanneer: ${wanneer.value}</li>
+    // </ul>
+    // `
+  }
 
-console.log(eventcontainer)
-}
-  console.log(eventTonen)
-function getevent(){
   console.log(eventcontainer)
- let getEvent =  JSON.parse(localStorage.getItem("evenementContainer"))
- console.log(getEvent)
+}
+console.log(eventTonen)
+function getevent() {
+  console.log(eventcontainer)
+  let getEvent = JSON.parse(localStorage.getItem("evenementContainer"))
+  console.log(getEvent)
 
- getEvent.forEach(element => {
+  getEvent.forEach(element => {
 
-//   eventTonen.appendChild('ul')
-//  let item = list.appendChild('li')
-//  let itemInhoud = item.textContent(element)
+    //   eventTonen.appendChild('ul')
+    //  let item = list.appendChild('li')
+    //  let itemInhoud = item.textContent(element)
 
-//om de datum juist te zetten volgens onze notatie, uitgeschreven met voluit de maanden!!!
-//getdate() - getMonth() -getYear()
-eventTonen.innerHTML +=
-   `<ul>
+    //om de datum juist te zetten volgens onze notatie, uitgeschreven met voluit de maanden!!!
+    //getdate() - getMonth() -getYear()
+    eventTonen.innerHTML +=
+      `<ul>
    <li>wat: ${element.wat}</li>
  <li>Waar: ${element.waar}</li>
 <li>Wanneer: ${element.wanneer}</li>
  </ul>
 `
-// console.log(itemInhoud)
- })
+    // console.log(itemInhoud)
+  })
 
 }
 
-function voegTaakToe(){
-  console.log("taak toegevoegd")
+//takenlijst - draaiboek
+let takenlijst = []
+let naamTaak = document.getElementById('naamTaak')
+let deadline = document.getElementById('deadline')
+let aantalPersonen = document.getElementById('aantalPersonen')
+let opmerkingen = document.getElementById('opmerkingen')
+let taken = document.getElementById('taken')
+
+function voegTaakToe() {
+  takenlijst.push({
+    'naamTaak': naamTaak.value,
+    'deadline': deadline.value,
+    'aantalPersonen': aantalPersonen.value,
+    'opmerkinge': opmerkingen
+  })
+  console.log(takenlijst)
+  localStorage.setItem("taakContainer", JSON.stringify(takenlijst))
+taken.innerHTML +=
+`<tr>
+<td>${naamTaak.value}</td>
+<td>${deadline.value}</td>
+<td>${aantalPersonen.value}</td>
+<td>${opmerkingen.value}</td>
+</tr>`
 }
 
